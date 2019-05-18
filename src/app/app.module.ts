854417b75eba0779b9b47e7d6129a87985fcdec5
KamilKwiecien <kmail.kwiecien@gmail.com>
2019-05-18 10:39:47 +0200

    Dopisany routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AppRoutingModule } from './app.routing.module';
 import { HttpMoviesService } from './service/http-movies.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -5,16 +6,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [HttpMoviesService],
   bootstrap: [AppComponent]
