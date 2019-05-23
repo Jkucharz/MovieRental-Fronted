dad7152b94853777b16acebfa0cd3e9508235084
Jakub Kucharz <kucharzj@gmail.com>
2019-05-23 22:09:51 +0200

    Dodany navbar, header i stopka

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
@@ -14,6 +17,9 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     MoviesComponent,
     HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
