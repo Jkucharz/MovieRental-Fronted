f66f41d29acfa523a4bf31c7d60ddf9841fd254f
KamilKwiecien <kmail.kwiecien@gmail.com>
2019-06-01 00:25:37 +0200

    Zabezpieczony panel sterowania

--- src/app/navbaradmin/navbaradmin.component.ts
+++ src/app/navbaradmin/navbaradmin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-navbaradmin',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbaradmin.component.css']
 })
 export class NavbaradminComponent implements OnInit {
+  loggedAdmin;
 
-  constructor() { }
+  constructor(private authService :AuthService) { }
 
   ngOnInit() {
+    this.subcribeVariable();
+  }
+
+  private subcribeVariable(){
+    this.authService.setUserName();
+     this.authService.getLoggedAdmin().subscribe(value=>{
+       this.loggedAdmin = value;
+     });
   }
 
 }
