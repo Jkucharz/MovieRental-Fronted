1e681904844eeb5052ac7851c05f497adddc62a5
KamilKwiecien <kmail.kwiecien@gmail.com>
2019-06-04 17:47:41 +0200

    fix: odświeżanie stron

--- src/app/service/type.service.ts
+++ src/app/service/type.service.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -7,33 +8,35 @@ import { Observable } from 'rxjs';
 })
 export class TypeService {
 
-  constructor(private http: HttpClient){ }
-  
+  constructor(private http: HttpClient, private router: Router) { }
+
   getAllTypes(): Observable<Array<Type>> {
-    return this.http.get<Array<Type>>('http://localhost:8080/movie/type',{});
+    return this.http.get<Array<Type>>('http://localhost:8080/movie/type', {});
   }
 
-  addMovieType(name: string){
+  addMovieType(name: string) {
     let headers = new HttpHeaders().set('Authorization', 'bearer  ' + localStorage.getItem('token'));
     const addType: Type = ({
       name: name,
     });
     this.http.post<Type>('http://localhost:8080/admin/movie/type/add', addType, { headers: headers }).subscribe(post => {
-      window.location.reload();  
+      this.router.navigateByUrl('', { skipLocationChange: true }).then(() =>
+        this.router.navigate(["/admin/types"]));
     });
   }
 
-  deleteMovieType(name: string){
+  deleteMovieType(name: string) {
     let headers = new HttpHeaders().set('Authorization', 'bearer  ' + localStorage.getItem('token'));
     const deleteType: Type = ({
       name: name,
     });
     this.http.post<Type>('http://localhost:8080/admin/movie/type/delete', deleteType, { headers: headers }).subscribe(post => {
-      window.location.reload();  
+      this.router.navigateByUrl('', { skipLocationChange: true }).then(() =>
+      this.router.navigate(["/admin/types"]));
     });
   }
 }
 
-export interface Type{
+export interface Type {
   name?: string;
 }
